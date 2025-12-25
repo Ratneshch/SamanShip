702aae85f1203392a27566280bf64729fffb992b
Ratnesh Chaurasiya <ratneshchaurasiya7809@gmail.com>
2025-12-25 11:14:28 +0530

    mognodb done register done

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,33 @@
+import connectDb from "@/lib/db";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req:NextRequest){
+try {
+  await connectDb();
+  const {name,email,password}=await req.json();
+  const existUser=await User.findOne({email});
+  if(existUser){
+    return NextResponse.json({message:"User already exists"},{status:400})
+  }
+  if(password.length<6){
+    return NextResponse.json({message:"Password must be at least 6 characters"},{status:400})
+  }
+  const hashedPassword= await bcrypt.hash(password,10)
+
+  const user=await User.create({
+    name,
+    email,
+    password:hashedPassword
+  })
+  return NextResponse.json({message:"User registered successfully",user},{status:201})
+
+} catch (error) {
+     return NextResponse.json({
+      message:`Internal Server Error: ${error}`
+     },{
+      status:500
+     })
+} 
+}
